76c83a12b4fa6d3f3ff0c2625cb5f4ff363150f9
nucuYu3Bcc22 <wmc468t80cxt@gmail.com>
2023-01-17 14:13:12 +0000

    Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(NewsController::class)->group(function() {
+    Route::get('admin/news/create', 'add');
+});
+
+
+#PHP/Laravel 09 Routingについて理解する
+#「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+#    Route::controller(AAAController::class)->group(function()
+#
+#{
+#    Route::get('XXX', 'bbb');
+#    
+#});
+
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('admin/profile/create', 'add');
+    Route::get('admin/profile/edit', 'edit');
+
+});
