93414850b5afb83f24036baeaebe02f3410d858d
nucuYu3Bcc22 <wmc468t80cxt@gmail.com>
2023-01-21 04:38:10 +0000

    Laravel

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -18,25 +18,25 @@
                     <div class="form-group row">
                         <label class="col-md-2">氏名</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body1" rows="1">{{ old('body1') }}</textarea>
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
                         </div>
                     </div>
                      <div class="form-group row">
                         <label class="col-md-2">性別</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body2" rows="1">{{ old('body2') }}</textarea>
+                            <input type="text" class="form-control" name="gender" value="{{ old('title') }}">
                         </div>
                     </div>
                      <div class="form-group row">
                         <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body3" rows="1">{{ old('body3') }}</textarea>
+                            <input type="text" class="form-control" name="hobby" value="{{ old('title') }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body4" rows="20">{{ old('body4') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('body4') }}</textarea>
                         </div>
                     </div>
                     @csrf
