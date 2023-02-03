b5c69c0edbca7cb960dba838631b39831f5737b6
nucuYu3Bcc22 <wmc468t80cxt@gmail.com>
2023-02-03 11:25:28 +0000

    Laravel

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,8 +2,8 @@
 
 
 @section('content')
-    <div class="container-fluid">
-            <div class="col-md-10 mx-auto">
+        <div class="container">
+            <div class="col">
                 <hr color="#c0c0c0">
                 <div class="report-container">
                     <h2><?php echo $data->name; ?>の天気予報</h2>
@@ -15,8 +15,8 @@
                     <div class="weather-forecast">
                         <img
                             src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
-                            class="weather-icon" />最高温度 <?php echo $data->main->temp_max; ?> °C / <span
-                            class="min-temperature">最低温度 <?php echo $data->main->temp_min; ?> °C</span>
+                            class="weather-icon" />最高温度 <?php echo $data->main->temp_max; ?> °C / 
+                            <span class="min-temperature">最低温度 <?php echo $data->main->temp_min; ?> °C</span>
                     </div>
                     <div class="time">
                         <div>湿度: <?php echo $data->main->humidity; ?> %</div>
@@ -24,63 +24,63 @@
                     </div>
                 </div>
             </div>
-        </div>
+        
     
-    <div class="container">
-        <hr color="#c0c0c0">
-        @if (!is_null($headline))
-            <div class="row">
-                <div class="headline col-md-10 mx-auto">
+            <!--<div class="container">-->
+                <hr color="#c0c0c0">
+                @if (!is_null($headline))
                     <div class="row">
-                        <div class="col-md-6">
-                            <div class="caption mx-auto">
-                                <h2><strong>最新のヘッドライン</strong></h2> 
-                                
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ secure_asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
+                        <div class="headline col-md-10 mx-auto">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="caption mx-auto">
+                                        <h2><strong>最新のヘッドライン</strong></h2> 
+                                        
+                                        <div class="image">
+                                            @if ($headline->image_path)
+                                                <img src="{{ secure_asset('storage/image/' . $headline->image_path) }}">
+                                            @endif
+                                        </div>
+                                        <div class="title p-2">
+                                            <h1>{{ Str::limit($headline->title, 70) }}</h1>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="title p-2">
-                                    <h1>{{ Str::limit($headline->title, 70) }}</h1>
+                                <div class="col-md-6">
+                                    <p class="body mx-auto">{{ Str::limit($headline->body, 650) }}</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <p class="body mx-auto">{{ Str::limit($headline->body, 650) }}</p>
-                        </div>
+                    </div>
+                @endif
+                <hr color="#c0c0c0">
+                <div class="row">
+                    <div class="posts col-md-8 mx-auto mt-3">
+                        @foreach($posts as $post)
+                            <div class="post">
+                                <div class="row">
+                                    <div class="text col-md-6">
+                                        <div class="date">
+                                            {{ $post->updated_at->format('Y年m月d日') }}
+                                        </div>
+                                        <div class="title">
+                                            {{ Str::limit($post->title, 150) }}
+                                        </div>
+                                        <div class="body mt-3">
+                                            {{ Str::limit($post->body, 1500) }}
+                                        </div>
+                                    </div>
+                                    <div class="image col-md-6 text-right mt-4">
+                                        @if ($post->image_path)
+                                            <img src="{{ secure_asset('storage/image/' . $post->image_path) }}">
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                            <hr color="#c0c0c0">
+                        @endforeach
                     </div>
                 </div>
             </div>
-        @endif
-        <hr color="#c0c0c0">
-        <div class="row">
-            <div class="posts col-md-8 mx-auto mt-3">
-                @foreach($posts as $post)
-                    <div class="post">
-                        <div class="row">
-                            <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
-                                <div class="title">
-                                    {{ Str::limit($post->title, 150) }}
-                                </div>
-                                <div class="body mt-3">
-                                    {{ Str::limit($post->body, 1500) }}
-                                </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ secure_asset('storage/image/' . $post->image_path) }}">
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <hr color="#c0c0c0">
-                @endforeach
-            </div>
         </div>
-    </div>
-    
 @endsection
